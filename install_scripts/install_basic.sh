abf4ed89776bda25711c87f32e77fefcafc7baa6
Ofir Gal <ofirgal753@gmail.com>
2021-11-16 15:40:05 +0200

    added gnome tweak to installation

--- install_scripts/install_basic.sh
+++ install_scripts/install_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Installing Basic Libs'
-sudo apt install -y xclip wget moreutils tmux ipython3
+sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool
 
 sudo usermod -a -G dialout $USER
 newgrp dialout
