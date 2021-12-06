5aef9112a9ec558d49faea5726a7bc55cf2f0567
Ofir Gal <ofirgal753@gmail.com>
2021-12-06 21:19:05 +0200

    added sshfs to install libs

--- install_scripts/install_basic.sh
+++ install_scripts/install_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Installing Basic Libs'
-sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool
+sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool sshfs
 
 sudo usermod -a -G dialout $USER
 newgrp dialout
