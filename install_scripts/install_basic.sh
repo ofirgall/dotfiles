6ef069ad8d7a745a21c106f115cd2e10a61ec376
Ofir Gal <ofirgal753@gmail.com>
2021-12-07 19:45:15 +0200

    added alias for open, added backup for dconf, added more basic libs

--- install_scripts/install_basic.sh
+++ install_scripts/install_basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Installing Basic Libs'
-sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool pcregrep sshfs
+sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool pcregrep sshfs python3-pip build-essential
 
 sudo usermod -a -G dialout $USER
 newgrp dialout
