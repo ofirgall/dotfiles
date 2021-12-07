3506b69efebb01b7b8d386fe90b3718866928a69
Ofir Gal <ofirgal753@gmail.com>
2021-12-07 09:37:25 +0200

    fixed install zsh script

--- install_scripts/install_zsh.sh
+++ install_scripts/install_zsh.sh
@@ -15,13 +15,17 @@ OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-  echo 'Updating oh-my-zsh'
-  upgrade_oh_my_zsh
 fi
 
 # Install zsh plugins
 mkdir -p ~/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/paulirish/git-open.git ~/.oh-my-zsh/custom/plugins/git-open
+
+install_plugin() {
+  if [ ! -d "$2" ] ; then
+    git clone "$1" "$2"
+  fi
+}
+install_plugin https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+install_plugin https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+install_plugin https://github.com/paulirish/git-open.git ~/.oh-my-zsh/custom/plugins/git-open
+
