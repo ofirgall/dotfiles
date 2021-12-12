6050890d170b96cd051d84e2861774e233d602d2
Ofir Gal <ofirgal753@gmail.com>
2021-12-12 09:37:05 +0200

    added bat

--- install_scripts/install_basic.sh
+++ install_scripts/install_basic.sh
@@ -3,5 +3,10 @@
 echo 'Installing Basic Libs'
 sudo apt install -y xclip wget moreutils tmux ipython3 gnome-tweak-tool pcregrep sshfs python3-pip build-essential
 
+# Install bat
+sudo apt install -y bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
 sudo usermod -a -G dialout $USER
 newgrp dialout
