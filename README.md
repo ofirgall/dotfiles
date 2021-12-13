f23c959b14feeed8873fc4c6ecc6b8bd0d2b29d3
Ofir Gal <ofirgal753@gmail.com>
2021-12-13 16:17:22 +0200

    added remote config

--- README.md
+++ README.md
@@ -21,7 +21,12 @@ git config user.email "ofirgal753@gmail.com"
 git config user.user "Ofir Gal"
 ```
 
-### Clone and Run
+### Clone and Run for Remote
+```bash
+git clone https://github.com/ofir753/dotfiles.git && cd dotfiles && sudo echo a && ./install --config-file remote.conf.yaml
+```
+
+### Clone and Run for PC
 ```bash
 git clone https://github.com/ofir753/dotfiles.git && cd dotfiles && sudo echo a && ./install
 ```
@@ -31,6 +36,5 @@ Incase you are using windows terminal add `windows_terminal_binds.json` to your 
 ---
 
 ## TODO:
-* remote config
 * tmux suspend shows in left status too
 * tmux suspend copy from remote and not to remote
--- install_scripts/install_zsh.sh
+++ install_scripts/install_zsh.sh
@@ -10,22 +10,3 @@ else
   echo 'Already using zsh'
 fi
 
-# Check if oh-my-zsh is installed
-OMZDIR="$HOME/.oh-my-zsh"
-if [ ! -d "$OMZDIR" ]; then
-  echo 'Installing oh-my-zsh'
-  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-# Install zsh plugins
-mkdir -p ~/.oh-my-zsh/custom/plugins
-
-install_plugin() {
-  if [ ! -d "$2" ] ; then
-    git clone "$1" "$2"
-  fi
-}
-install_plugin https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-install_plugin https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-install_plugin https://github.com/paulirish/git-open.git ~/.oh-my-zsh/custom/plugins/git-open
-
