5700952dc35dcdbd33d414b343d97a49a1df4bf4
Ofir Gal <ofirgal753@gmail.com>
2022-01-16 09:08:18 +0200

    installing packer

--- install_scripts/install_nvim.sh
+++ install_scripts/install_nvim.sh
@@ -6,9 +6,9 @@ chmod +x $HOME/.local/bin/nvim
 # Install external dependecies (rg is in install_basic)
 sudo apt install -w fd-find
 
-# install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# install packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # install xkb-switch
 sudo apt-get install -y libglib2.0-dev
