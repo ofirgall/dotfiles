b28266626814e477154d592a9d9c79a402041364
Ofir Gal <ofirgal753@gmail.com>
2022-02-06 19:37:07 +0200

    escaping C+J/H in fzf, reverse search height

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ rustup update
 python3 -m pip install pip --upgrade
 python3 -m pip install -r dotfiles_scripts/requirements.txt --user
 
-# TODO: Update oh-my-zsh custom plugins (maybe move to zsh plugin manager e.g zinit)
+# TODO: Update oh-my-zsh custom plugins (maybe move to zsh plugin manager e.g zinit, antigen, zplug)
