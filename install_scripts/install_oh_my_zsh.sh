b57fb26a3b84d611cf2a4d5d20a919ebab4d619a
Ofir Gal <ofirgal753@gmail.com>
2022-02-11 22:38:31 +0200

    removed unused plugins from oh my zsh

--- install_scripts/install_oh_my_zsh.sh
+++ install_scripts/install_oh_my_zsh.sh
@@ -7,9 +7,6 @@ if [ ! -d "$OMZDIR" ]; then
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-# Install zsh plugins dependcies
-sudo apt install -y sqlite3 # zsh-histdb
-
 # Install zsh plugins
 mkdir -p ~/.oh-my-zsh/custom/plugins
 
@@ -22,6 +19,4 @@ install_plugin https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/cus
 install_plugin https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 install_plugin https://github.com/paulirish/git-open.git ~/.oh-my-zsh/custom/plugins/git-open
 install_plugin https://github.com/peterhurford/up.zsh ~/.oh-my-zsh/custom/plugins/up
-install_plugin https://github.com/jeffreytse/zsh-vi-mode ~/.oh-my-zsh/custom/plugins/zsh-vi-mode
-install_plugin https://github.com/larkery/zsh-histdb ~/.oh-my-zsh/custom/plugins/zsh-histdb
 install_plugin https://github.com/ofirgall/zsh-fzf-history-search ~/.oh-my-zsh/custom/plugins/zsh-fzf-history-search
