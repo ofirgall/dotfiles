52517f433955f47de63ff523372e89799fb2363e
Ofir Gal <ofirgal753@gmail.com>
2022-02-11 18:07:22 +0200

    improved tmux select session list

--- dotfiles_scripts/select_tmux_session.sh
+++ dotfiles_scripts/select_tmux_session.sh
@@ -7,21 +7,23 @@ fi
 
 select_tmux_session()
 {
-	session=$(tmux ls -F '#S' | fzf --reverse --header="Select Tmux Session. Ctrl-C to create new session.")
+	session=$(tmux ls -F '#S' | fzf --reverse --header="Select Tmux Session. Ctrl-f to Create New Session, Ctrl-C to exit." --bind "ctrl-f:abort+execute(echo ___new_session)")
 
 	# ctrl-c
 	if [ -z $session ]; then
 		return
 	fi
-	tmux attach -t "$session"
+
+	if [ "$session" == "___new_session" ]; then
+		tmux new-session -s "new-session-$(uuidgen | cut -c1-8)"
+	else
+		# Attach to session
+		tmux attach -t "$session"
+	fi
+
 	if [[ "$ZSH_TMUX_ALWAYS_SELECT_SESSION" == "true" ]]; then
 		select_tmux_session
 	fi
 }
 
 select_tmux_session
-
-if [[ "$ZSH_TMUX_ALWAYS_OPEN_SESSION" == "true" ]]; then
-	session_name="new-session-$(uuidgen | cut -c1-8)"
-	tmux new-session -s $session_name
-fi
