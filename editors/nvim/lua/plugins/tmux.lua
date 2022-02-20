505d44a65375b55c3bc2cc913ac28563c739800e
Ofir Gal <ofirgal753@gmail.com>
2022-02-20 08:52:19 +0200

    auto rename after quit nvim

--- editors/nvim/lua/plugins/tmux.lua
+++ editors/nvim/lua/plugins/tmux.lua
@@ -1,5 +1,5 @@
 
 vim.cmd([[
 autocmd BufEnter * call system("tmux rename-window 'nv:" . fnamemodify(getcwd(), ":t") . "'")
-autocmd QuitPre * call system("tmux rename-window zsh")
+autocmd QuitPre * call system("tmux set -w automatic-rename on")
 ]])
