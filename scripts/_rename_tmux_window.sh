fb0301a6716ce0ab6c014b1d4d591fd433996d60
Ofir Gal <ofirgal753@gmail.com>
2022-02-22 13:01:48 +0200

    limiting dirname in tmux window name

--- scripts/_rename_tmux_window.sh
+++ scripts/_rename_tmux_window.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+PROGRAM_LIMIT=20
+PATH_LIMIT=10
+
 pane_id=$1
 program=$(ps -f --no-headers --ppid $pane_id | awk '{ print substr($0, index($0,$8)) }')
 
 # No program running, print cd
 if [ -z "$program" ]; then
 	path=$(tmux display-message -p \#{pane_current_path})
-	D2=$(dirname "$path")
-	DIRNAME2=$(basename "$D2")/$(basename "$path")
-	echo $DIRNAME2
+	last_dir=$(basename "$path")
+	second_last_dir=$(basename "$(dirname $path)")
+	echo "${second_last_dir:0:$PATH_LIMIT}/${last_dir:0:$PATH_LIMIT}"
 else
-	echo ${program:0:20}
+	echo ${program:0:$PROGRAM_LIMIT}
 fi
