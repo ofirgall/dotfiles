f2b37aeb95c58a1a37c692e4de3da63586955496
Ofir Gal <ofirgal753@gmail.com>
2022-02-22 13:27:34 +0200

    improved cg script

--- scripts/inner_cg.sh
+++ scripts/inner_cg.sh
@@ -11,6 +11,15 @@ ls_with_branch()
 
 choose_dir()
 {
+	# No dirs
+	if ! ls -d */ &> /dev/null; then
+		return
+	fi
+
+	if [ -d ".git" ]; then
+		return
+	fi
+
 	result=$(ls_with_branch | fzf --reverse --header="$(pwd)/ (Ctrl-C to abort, Ctrl-X to exit)" --height=30 --bind "ctrl-x:abort+execute(echo ___exit)")
 
 	# ctrl-c
@@ -26,12 +35,13 @@ choose_dir()
 
 	path=$(echo "$result" | sed "s/ (.*//")
 	cd "$path"
-	if [ -d ".git" ]; then
-		return
-	fi
 	choose_dir
 }
 
-cd $START_DIR
+if [ "$#" -eq 1 ]; then
+	cd $1
+else
+	cd $START_DIR
+fi
 choose_dir
-pwd
+pwd # echo pwd at end (to cd later)
