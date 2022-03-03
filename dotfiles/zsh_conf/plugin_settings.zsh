e8696dc062f213db3973d292eff58750c1949c60
Ofir Gal <ogal@infinidat.com>
2022-03-03 18:56:20 +0200

    fzf-tab disabled for remote incompatiable shells

--- dotfiles/zsh_conf/plugin_settings.zsh
+++ dotfiles/zsh_conf/plugin_settings.zsh
@@ -7,19 +7,21 @@ ZSH_TMUX_AUTOSTART_ONCE=false
 ZSH_TMUX_AUTOQUIT=false
 ZSH_TMUX_FIXTERM=false
 
-# ---------------------------
-#		  FZF Tab
-# ---------------------------
-# disable sort when completing `git checkout`
-zstyle ':completion:*:git-checkout:*' sort false
-# set descriptions format to enable group support
-zstyle ':completion:*:descriptions' format '[%d]'
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-# preview directory's content with exa when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -1 --color=always $realpath'
-# switch group using `,` and `.`
-zstyle ':fzf-tab:*' switch-group ',' '.'
+if [[ ${ZSH_VERSION:0:3} -ge 5.8 ]]; then
+	# ---------------------------
+	#		  FZF Tab
+	# ---------------------------
+	# disable sort when completing `git checkout`
+	zstyle ':completion:*:git-checkout:*' sort false
+	# set descriptions format to enable group support
+	zstyle ':completion:*:descriptions' format '[%d]'
+	# set list-colors to enable filename colorizing
+	zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+	# preview directory's content with exa when completing cd
+	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls -1 --color=always $realpath'
+	# switch group using `,` and `.`
+	zstyle ':fzf-tab:*' switch-group ',' '.'
+fi
 
 # ---------------------------
 #		  FZF History
