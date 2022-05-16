0aa90d367be97fdfc768295dc724fceba5ef1dea
Ofir Gal <ofirgal753@gmail.com>
2022-05-16 11:02:29 +0300

    added autopair on completion

--- editors/nvim/lua/plugins/autocomplete.lua
+++ editors/nvim/lua/plugins/autocomplete.lua
@@ -125,6 +125,10 @@ cmp.setup.cmdline(':', {
 	})
 })
 
+-- add pair when accepting autocomplet
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
 -- LSP SAGA --
 -- Disable builtin diagnostic
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
