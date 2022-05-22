9afe66339119fb6fdf76f732d86e6a3efc8cb9ae
Ofir Gal <ofirgal753@gmail.com>
2022-05-22 14:42:13 +0300

    zsh-histdb only for 5.1>

--- dotfiles/zsh_conf/plugin_settings.zsh
+++ dotfiles/zsh_conf/plugin_settings.zsh
@@ -36,18 +36,19 @@ export AUTO_NOTIFY_THRESHOLD=300 # 5 minutes
 # ---------------------------
 #	histdb integ for zsh-autosuggestions
 # ---------------------------
-_zsh_autosuggest_strategy_histdb_top() {
-    local query="
-        select commands.argv from history
-        left join commands on history.command_id = commands.rowid
-        left join places on history.place_id = places.rowid
-        where commands.argv LIKE '$(sql_escape $1)%'
-        group by commands.argv, places.dir
-        order by places.dir != '$(sql_escape $PWD)', count(*) desc
-        limit 1
-    "
-    suggestion=$(_histdb_query "$query")
-}
+if [[ ${ZSH_VERSION:0:3} -ge 5.1 ]]; then
+	_zsh_autosuggest_strategy_histdb_top() {
+		local query="
+			select commands.argv from history
+			left join commands on history.command_id = commands.rowid
+			left join places on history.place_id = places.rowid
+			where commands.argv LIKE '$(sql_escape $1)%'
+			group by commands.argv, places.dir
+			order by places.dir != '$(sql_escape $PWD)', count(*) desc
+			limit 1
+		"
+		suggestion=$(_histdb_query "$query")
+	}
 
-ZSH_AUTOSUGGEST_STRATEGY=histdb_top
-
+	ZSH_AUTOSUGGEST_STRATEGY=histdb_top
+fi
