226f0e82ca26ed22d12f16cac7f98d348c5d0908
Ofir Gal <ofirgal753@gmail.com>
2022-06-08 18:22:47 +0300

    indent blankline to use treesitter

--- editors/nvim/lua/plugins/design.lua
+++ editors/nvim/lua/plugins/design.lua
@@ -348,7 +348,7 @@ monokai.setup {
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 require("indent_blankline").setup {
-	indent_blankline_use_treesitter = true,
+	use_treesitter = true,
 	show_trailing_blankline_indent = false,
 	space_char_blankline = " ",
 }
