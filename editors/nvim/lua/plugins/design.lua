b174ede08eb0eecc8c115c42beb75ae2c2a5a27e
Ofir Gal <ofirgal753@gmail.com>
2022-07-18 14:35:52 +0300

    visual and search bg are differenet now

--- editors/nvim/lua/plugins/design.lua
+++ editors/nvim/lua/plugins/design.lua
@@ -123,7 +123,6 @@ local tree_bg = '#1a1a18'
 local telescope_bg =  '#181816'-- Slightly darker than tree_bg
 local tab_visible_fg = '#b3ab60'
 local cursor_line_bg = '#2e2e27'
-local visual_bg = '#555449'
 local orange = '#de933c'
 local telescope_prompt = '#1d1d1a'
 monokai.setup {
@@ -246,10 +245,11 @@ monokai.setup {
 			-- style = 'reverse',
 		},
 		Search = {
-			bg = visual_bg,
+			bg = '#3b3a30',
+			style = 'underline'
 		},
 		Visual = {
-			bg = visual_bg,
+			bg = '#46453a',
 		},
 		-- telescope.nvim
 		TelescopeBorder = {
