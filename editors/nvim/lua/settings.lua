69991f08b41ea4e8c3edc356cc0f2def75ddf3f4
Ofir Gal <ofirgal753@gmail.com>
2022-07-25 16:07:34 +0300

    always show git changes

--- editors/nvim/lua/settings.lua
+++ editors/nvim/lua/settings.lua
@@ -18,6 +18,7 @@ opt.updatetime = 100 -- mainly for trld.nvim which utilize CursorHold autocmd
 opt.formatoptions:append('cro') -- continue comments when going down a line
 opt.mouse = 'a' -- Enable mouse when guest are using my nvim
 opt.foldlevelstart = 99 -- no auto folding
+opt.signcolumn = 'auto:2' -- Enable 2 signs in the column
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
