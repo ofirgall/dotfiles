2bb27878adaf931822ef38fc5b56d60a119482d0
Ofir Gal <ofirgal753@gmail.com>
2022-07-25 15:36:29 +0300

    better stop sign for dap

--- editors/nvim/lua/plugins/debug.lua
+++ editors/nvim/lua/plugins/debug.lua
@@ -28,6 +28,7 @@ vim.fn.sign_define('DapBreakpoint', {text='', texthl='DiagnosticError', lineh
 vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='DiagnosticWarn', linehl='', numhl=''})
 vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='DiagnosticWarn', linehl='', numhl=''})
 vim.fn.sign_define('DapLogPoint', {text='', texthl='DiagnosticWarn', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='', texthl='DiagnosticInfo', linehl='CursorLine', numhl='CursorLine'})
 
 dap.configurations.go = {
 	{
