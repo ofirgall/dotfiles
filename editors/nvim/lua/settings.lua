0334932b5211d590dbdc7171bfdb09cf723ca32d
Ofir Gal <ofirgal753@gmail.com>
2022-08-02 12:57:52 +0300

    fixed wsl nvim clipboard

--- editors/nvim/lua/settings.lua
+++ editors/nvim/lua/settings.lua
@@ -31,9 +31,11 @@ if vim.fn.has('wsl') == 1 then
 		name = "win32yank-wsl",
 		copy = {
 			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
 		},
 		paste = {
 			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
 		},
 		cache_enabled = false
 	}
