b9c365a2fc6f5a241190cc58036d8ea43d55fe10
Ofir Gal <ofirgal753@gmail.com>
2022-08-02 17:08:19 +0300

    installing codelddb

---
+++ install_scripts/install_dap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e # Exit if fail
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$CURRENT_DIR/helpers.sh"
+
+install_codelldb()
+{
+	unzip codelldb-x86_64-linux.vsix
+	ln -s $HOME/.local/dap/codelldb/extension/adapter/codelldb $HOME/.local/bin/codelldb
+}
+
+# C/C++/Rust debug adapter
+download_latest_release $HOME/.local/dap/codelldb vadimcn/vscode-lldb codelldb-x86_64-linux.vsix install_codelldb
