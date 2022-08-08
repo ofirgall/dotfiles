0e59757cc20c29c1a441d59b1fb3946098ecc4f0
Ofir Gal <ofirgal753@gmail.com>
2022-08-08 16:19:41 +0300

    recenter after jump

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -26,28 +26,24 @@ require'nvim-treesitter.configs'.setup {
 			set_jumps = true, -- whether to set jumps in the jumplist
 			goto_next_start = {
 				["]m"] = "@function.outer",
-				["gj"] = "@function.outer",
 				["]]"] = "@class.outer",
 				["]b"] = "@block.outer",
 				["]a"] = "@parameter.inner",
 			},
 			goto_next_end = {
 				["]M"] = "@function.outer",
-				["gJ"] = "@function.outer",
 				["]["] = "@class.outer",
 				["]B"] = "@block.outer",
 				["]A"] = "@parameter.inner",
 			},
 			goto_previous_start = {
 				["[m"] = "@function.outer",
-				["gk"] = "@function.outer",
 				["[["] = "@class.outer",
 				["[b"] = "@block.outer",
 				["[a"] = "@parameter.inner",
 			},
 			goto_previous_end = {
 				["[M"] = "@function.outer",
-				["gK"] = "@function.outer",
 				["[]"] = "@class.outer",
 				["[B"] = "@block.outer",
 				["[A"] = "@parameter.inner",
