f0e43354c0a9db38552a5f39f7354551fbabb878
Ofir Gal <ofirgal753@gmail.com>
2022-08-21 14:29:03 +0300

    enabled toggle deleted

--- editors/nvim/lua/plugins/git.lua
+++ editors/nvim/lua/plugins/git.lua
@@ -30,7 +30,7 @@ gs.setup {
 		map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
 		map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
 		map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-		-- map('n', '<leader>gd', '<cmd>Gitsigns toggle_deleted<CR>')
+		map('n', '<leader>hd', '<cmd>Gitsigns toggle_deleted<CR>')
 		-- Text object
 		map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 	end
