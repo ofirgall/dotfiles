491c4eead059ad1f15a83eab5094d886081f0f51
Ofir Gal <ofirgal753@gmail.com>
2022-09-18 14:53:46 +0300

    zsh hook only in tmux session

--- dotfiles/zsh_conf/hooks.zsh
+++ dotfiles/zsh_conf/hooks.zsh
@@ -3,4 +3,6 @@ tmux-window-name() {
 	($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
 }
 
-add-zsh-hook chpwd tmux-window-name
+if [ -n "$TMUX_PLUGIN_MANAGER_PATH" ]; then
+	add-zsh-hook chpwd tmux-window-name
+fi
