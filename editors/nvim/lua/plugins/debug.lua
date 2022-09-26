fd389d2c2361752bb625c29bd1587a4487e6264a
Ofir Gal <ofirgal753@gmail.com>
2022-09-26 11:26:05 +0300

    debug.lua cleanup

--- editors/nvim/lua/plugins/debug.lua
+++ editors/nvim/lua/plugins/debug.lua
@@ -1,28 +1,7 @@
-local dapui = require('dapui')
-dapui.setup {
-	expand_lines = false,
-	layouts = {
-		{
-			size = 0.25,
-			position = 'bottom',
-			elements = {
-				'scopes' -- local vars
-			}
-		},
-		{
-			size = 0.20,
-			position = 'left',
-			elements = {
-				'breakpoints',
-				'watches',
-				'stacks',
-			}
-		},
-	}
-}
-
--- DAP Config
+-- mfussenegger/nvim-dap
 local dap = require('dap')
+
+--- Signs ---
 -- Sign priority = 11
 vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticError', linehl = '', numhl = '' })
 vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'DiagnosticWarn', linehl = '', numhl = '' })
@@ -48,10 +27,10 @@ dap.configurations.rust = dap.configurations.cpp
 
 dap.adapters.codelldb = {
 	type = 'server',
-	port = "${port}",
+	port = '${port}',
 	executable = {
 		command = 'codelldb',
-		args = { "--port", "${port}" },
+		args = { '--port', '${port}' },
 	}
 }
 
@@ -76,21 +55,45 @@ dap.adapters.go = {
 	port = 2345,
 }
 
+-- rcarriga/nvim-dap-ui
+local dapui = require('dapui')
+dapui.setup {
+	expand_lines = false,
+	layouts = {
+		{
+			size = 0.25,
+			position = 'bottom',
+			elements = {
+				'scopes' -- local vars
+			}
+		},
+		{
+			size = 0.20,
+			position = 'left',
+			elements = {
+				'breakpoints',
+				'watches',
+				'stacks',
+			}
+		},
+	}
+}
+
 local function map(mode, l, r, opts)
 	opts = opts or { silent = true }
 	vim.keymap.set(mode, l, r, opts)
 end
 
 local dap_closed = function()
-	dapui.close()
-	vim.api.nvim_command("tabclose $") -- $(last) is the debug page
+	dapui.close({})
+	vim.api.nvim_command('tabclose $') -- $(last) is the debug page
 	map('n', '<RightMouse>', '<LeftMouse><cmd>Lspsaga hover_doc<cr>') -- Trigger hover
 end
 
--- Auto open and close dapui
+-- Hooks to dap, opens/cloes dap-ui binds rightlick to evaluate
 dap.listeners.after.event_initialized['dapui_config'] = function()
-	vim.api.nvim_command("$tabnew") -- $(last) is the debug page
-	dapui.open()
+	vim.api.nvim_command('$tabnew') -- $(last) is the debug page
+	dapui.open({})
 	map('n', '<RightMouse>', '<LeftMouse><cmd>lua require"dapui".eval()<cr>') -- Trigger hover
 end
 dap.listeners.before.event_terminated['dapui_config'] = function()
@@ -100,7 +103,7 @@ dap.listeners.before.event_exited['dapui_config'] = function()
 	dap_closed()
 end
 
--- automatically load breakpoints when a file is loaded into the buffer.
+-- Weissle/persistent-breakpoints.nvim
 require('persistent-breakpoints').setup {}
 vim.api.nvim_create_autocmd('BufReadPost', {
 	pattern = '*',
