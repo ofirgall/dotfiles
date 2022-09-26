7cc95a101241202d295d72c787de1d6c3142a8a8
Ofir Gal <ofirgal753@gmail.com>
2022-09-26 11:41:24 +0300

    cleanup easypick.lua

--- editors/nvim/lua/plugins/easypick.lua
+++ editors/nvim/lua/plugins/easypick.lua
@@ -1,10 +1,11 @@
+-- axkirillov/easypick.nvim
 local easypick = require('easypick')
 
 easypick.setup {
 	pickers = {
 		{
-			name = "dirtyfiles",
-			command = "git status -s | cut -c 4-",
+			name = 'dirtyfiles',
+			command = 'git status -s | cut -c 4-',
 			previewer = easypick.previewers.default()
 		},
 	}
