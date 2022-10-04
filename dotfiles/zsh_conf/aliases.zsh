2f128e696c18c4894e538b857600f6786fd8a5d7
Ofir Gal <ofir@gal.software>
2022-10-04 15:03:44 +0300

    added cd

--- dotfiles/zsh_conf/aliases.zsh
+++ dotfiles/zsh_conf/aliases.zsh
@@ -16,8 +16,10 @@ alias tkill='tmux kill-session'
 alias trename='tmux rename-session'
 alias todo='nvim ~/todo.norg'
 alias taskopen-fzf='taskopen -l | sed "s/ *[0-9]*) //" | sed "/^$/d" | fzf | sed "s/.*-- \([0-9]*\)/\1/" | sponge | { IFS= read -r x; { printf "%s\n" "$x"; cat; } | xargs taskopen }'
-alias cdn='cd $HOME/.local/share/nvim/site/pack/packer/start' # cd to neovim plugins
 alias cdd='cd $HOME/dotfiles'
+alias cdn='cd $HOME/dotfiles/editors/nvim/' # cd to neovim config
+alias cdp='cd $HOME/.local/share/nvim/site/pack/packer/start' # cd to packer/plugins
+alias cdt='cd $HOME/.local/share/nvim/site/pack/packer/start/ofirkai.nvim/' # cd to nvim theme
 alias g='fugitive' # git fugitive
 alias ngh='git_tree' # git history with nvim and Flog
 function cg() { cd $(inner_cg.sh $@) } # cd to git repos
