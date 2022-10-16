73a21f73df693caf99b6a7d5faf8fe14ac73fe23
Ofir Gal <ofir@gal.software>
2022-10-16 16:21:36 +0300

    lemmy-help

--- install_scripts/install_nvim.sh
+++ install_scripts/install_nvim.sh
@@ -4,6 +4,9 @@ set -e # Exit if fail
 
 sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 
+# numToStr/lemmy-help
+cargo install lemmy-help --features=cli
+
 if -d ~/.local/share/nvim; then
 	echo "Skipping nvim install"
 else
