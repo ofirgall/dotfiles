6465733e3264e000264cff272cda9ddf8656112d
Ofir Gal <ofir@gal.software>
2022-10-16 16:53:36 +0300

    fixed cmp_nvim_lsp deprecation

--- editors/nvim/lua/plugins/lsp.lua
+++ editors/nvim/lua/plugins/lsp.lua
@@ -6,8 +6,7 @@ end
 local lspconfig = require('lspconfig')
 
 -- Update capabilities to autocomplete
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.window.workDoneProgress = true
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lsp_signature_cfg = {
 	bind = true,
