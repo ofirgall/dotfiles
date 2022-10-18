3760a478075afddffa3e0dcfbc8bba4e38372820
Ofir Gal <ofir@gal.software>
2022-10-18 10:25:06 +0300

    open teams instead of slack

--- scripts/inner/_on_tmux_attach.sh
+++ scripts/inner/_on_tmux_attach.sh
@@ -7,6 +7,7 @@ fi
 current_session=$(tmux display-message -p '#S')
 
 if [ "$current_session" == "main" ]; then
-	slack # open slack
+	# slack # open slack
+	teams # open teams :(
 	$HOME/.tmux/plugins/tmux-browser/scripts/open_browser.sh # open browser (mail & calander)
 fi
