81a58cb551abaeeb9fe4de9547f60cf0a6567866
Ofir Gal <ofir@gal.software>
2022-11-03 09:54:35 +0200

    ignore toggleterm in whitespace checker

--- editors/nvim/lua/plugins/misc.lua
+++ editors/nvim/lua/plugins/misc.lua
@@ -372,3 +372,6 @@ require('nvim-surround').setup {
 	},
 	move_cursor = false,
 }
+
+-- ntpeters/vim-better-whitespace
+vim.g.better_whitespace_filetypes_blacklist = { 'diff', 'git', 'gitcommit', 'unite', 'qf', 'help', 'markdown', 'fugitive', 'toggleterm' }
