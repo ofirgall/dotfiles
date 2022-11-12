586989ca228d4dd303e3a0c8b5eccd468d1717f7
Ofir Gal <ofir@gal.software>
2022-11-12 14:16:48 +0200

    awesomewm-vim-tmux-navigator using Alt

--- awesomewm/plugins.lua
+++ awesomewm/plugins.lua
@@ -4,6 +4,6 @@ require("awesomewm-vim-tmux-navigator") {
     down = { "Down", "j" },
     left = { "Left", "h" },
     right = { "Right", "l" },
-    mod = "Mod4",
-    mod_keysym = "Super_L",
+    mod = "Mod1", -- Alt
+    mod_keysym = "Alt_L",
 }
