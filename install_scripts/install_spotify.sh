7c929f21df0fe2edaba376115d0a0839c8485a23
Ofir Gal <ofir@gal.software>
2022-11-14 09:47:33 +0200

    installing spotify from apt

--- install_scripts/install_spotify.sh
+++ install_scripts/install_spotify.sh
@@ -9,6 +9,12 @@ if $IS_REMOTE; then
     exit
 fi
 
+
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/spotify.gpg
+echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install -y spotify-client
+
 # Install jpochyla/psst
 rm -rf /tmp/psst
 mkdir /tmp/psst
