a4d686f61b47e872c83aff5976cebac9dc5bfc7f
Ofir Gal <ofir@gal.software>
2022-11-20 14:03:18 +0200

    disabled undo/redo messages from noice

--- editors/nvim/lua/ui.lua
+++ editors/nvim/lua/ui.lua
@@ -242,6 +242,80 @@ if not vim.g.started_by_firenvim then
 				},
 				opts = { skip = true },
 			},
+			{
+				-- Disable "written" messages
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
+			},
+			-- Disable "redo/undo" messages
+			{
+				filter = {
+					event = "msg_show",
+					kind = "lua_error",
+					find = "more line",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "lua_error",
+					find = "fewer line",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "lua_error",
+					find = "line less",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "lua_error",
+					find = "change;",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "more line",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "fewer line",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "line less",
+				},
+				opts = { skip = true },
+			},
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "change;",
+				},
+				opts = { skip = true },
+			},
 		},
 	}
 end
