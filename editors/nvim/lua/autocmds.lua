ad649aa37356c24759b5ef83a9d5b221fdbc388e
Ofir Gal <ofir@gal.software>
2022-11-21 13:22:05 +0200

    removed Focus events, I dont need them with awesome

--- editors/nvim/lua/autocmds.lua
+++ editors/nvim/lua/autocmds.lua
@@ -4,22 +4,6 @@ local opt = vim.opt
 
 config_autocmds = api.nvim_create_augroup('config', { clear = true })
 
--- Enable and disable mouse when gaining/losing focus to avoid the first click jump
-api.nvim_create_autocmd('FocusGained', {
-	group = config_autocmds,
-	pattern = '*',
-	callback = function()
-		opt.mouse = 'a'
-	end
-})
-api.nvim_create_autocmd('FocusLost', {
-	group = config_autocmds,
-	pattern = '*',
-	callback = function()
-		opt.mouse = ''
-	end
-})
-
 -- Highlight on yank
 api.nvim_create_autocmd('TextYankPost', {
 	group = config_autocmds,
