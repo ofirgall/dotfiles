d7a5a2dbb0a9a2b805b49b5b0b4efb2d679d606b
Ofir Gal <ofir@gal.software>
2022-12-15 10:17:22 +0200

    mouse speed for Glorious

--- awesomewm/autorun.sh
+++ awesomewm/autorun.sh
@@ -5,3 +5,6 @@ setxkbmap -option caps:escape # Capslock to escape
 
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 325 1 # Natural scrolling
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 348 1 # Tapping enabled
+
+# Mouse speed at home
+xinput --set-prop 'Glorious Model O Wireless' 'libinput Accel Speed' -0.3
