7554e6981699e668e691ecee7757081350f8ea65
Ofir Gal <ofir@gal.software>
2022-12-15 14:22:47 +0200

    awesome width control

--- awesomewm/keymaps.lua
+++ awesomewm/keymaps.lua
@@ -262,10 +262,10 @@ function M.setup(kbdcfg, volume_widget, retain)
         awful.key({ modkey, "Shift" }, "q", awesome.quit,
             { description = "quit awesome", group = "awesome" }),
 
-        awful.key({ modkey, "Control", }, "l", function() awful.tag.incmwfact(0.05) end,
+        awful.key({ modkey, "Control", }, "l", function() awful.tag.incmwfact(0.01) end,
             { description = "increase master width factor", group = "layout" }),
-        -- awful.key({ modkey, "Control", }, "h", function() awful.tag.incmwfact(-0.05) end,
-        --     { description = "decrease master width factor", group = "layout" }),
+        awful.key({ modkey, "Control", }, "h", function() awful.tag.incmwfact(-0.01) end,
+            { description = "decrease master width factor", group = "layout" }),
         -- awful.key({ modkey, "Shift" }, "h", function() awful.tag.incnmaster(1, nil, true) end,
         --     { description = "increase the number of master clients", group = "layout" }),
         -- awful.key({ modkey, "Shift" }, "l", function() awful.tag.incnmaster(-1, nil, true) end,
