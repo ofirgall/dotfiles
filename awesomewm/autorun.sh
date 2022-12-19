190ccb3a38010c5e3798f069edfbe3b36d8b72c6
Ofir Gal <ofir@gal.software>
2022-12-19 10:16:20 +0200

    added mouse speed at work

--- awesomewm/autorun.sh
+++ awesomewm/autorun.sh
@@ -9,4 +9,7 @@ xinput set-prop "DELL0A36:00 0488:101A Touchpad" 348 1 # Tapping enabled
 # Mouse speed at home
 xinput --set-prop 'Glorious Model O Wireless' 'libinput Accel Speed' -0.3
 
+# Mouse speed at work
+xinput --set-prop 'pointer:Logitech MX Master 3' 'libinput Accel Speed' -0.5
+
 xmodmap ~/.xmodmaprc 2> /dev/null # key mapping, xev to see keys
