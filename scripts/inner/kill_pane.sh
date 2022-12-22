bcaf0308c12a5ded199d21c92c439aec66013715
Ofir Gal <ofir@gal.software>
2022-12-22 15:08:32 +0200

    don't kill if last-window + last-pane

--- scripts/inner/kill_pane.sh
+++ scripts/inner/kill_pane.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 amount_of_panes=$(tmux list-panes | wc -l)
-if [ $amount_of_panes -eq 1 ]; then
+amount_of_windows=$(tmux list-windows | wc -l)
+
+if [ $amount_of_panes -eq 1 ] && [ $amount_of_windows -eq 1 ]; then
     exit
 fi
 
