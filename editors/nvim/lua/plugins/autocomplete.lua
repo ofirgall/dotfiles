922e152c1f026b9678d3e3e291e75051ba715205
Ofir Gal <ofir@gal.software>
2022-12-25 17:29:44 +0200

    variables above functions

--- editors/nvim/lua/plugins/autocomplete.lua
+++ editors/nvim/lua/plugins/autocomplete.lua
@@ -46,6 +46,30 @@ local function all_visible_buffers_source(priority)
 	}
 end
 
+local types = require('cmp.types')
+local compare = require('cmp.types')
+
+local kind_compare = function(entry1, entry2)
+	local kind1 = entry1:get_kind()
+	kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
+	local kind2 = entry2:get_kind()
+	kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
+	if kind1 ~= kind2 then
+		if kind1 == types.lsp.CompletionItemKind.Snippet or kind1 == types.lsp.CompletionItemKind.Variable then
+			return true
+		end
+		if kind1 == types.lsp.CompletionItemKind.Snippet or kind1 == types.lsp.CompletionItemKind.Variable then
+			return false
+		end
+		local diff = kind1 - kind2
+		if diff < 0 then
+			return true
+		elseif diff > 0 then
+			return false
+		end
+	end
+end
+
 cmp_setup_dict = {
 	snippet = {
 		expand = function(args)
@@ -114,6 +138,21 @@ cmp_setup_dict = {
 	performance = {
 		debounce = 30, -- default: 60
 		throttle = 15, -- default: 30
+	},
+	sorting = {
+		comparators = {
+			compare.offset,
+			compare.exact,
+			-- compare.scopes,
+			compare.score,
+			compare.recently_used,
+			compare.locality,
+			kind_compare, -- compare.kind,
+			compare.sort_text,
+			compare.length,
+			compare.order,
+		},
+
 	},
 	preselect = cmp.PreselectMode.None, -- Auto select the first item
 
