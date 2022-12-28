f75f2a0abd233d7eea92148532fbd3a5c02924bb
Ofir Gal <ofir@gal.software>
2022-12-28 18:03:20 +0200

    telescope recenter

--- editors/nvim/lua/plugins/telescope.lua
+++ editors/nvim/lua/plugins/telescope.lua
@@ -11,11 +11,19 @@ require('telescope').setup {
 				['<C-k>'] = 'move_selection_previous',
 				['<C-n>'] = 'cycle_history_next',
 				['<C-p>'] = 'cycle_history_prev',
+				['<CR>'] = require('telescope.actions').select_default + require('telescope.actions').center,
+				['<C-x>'] = require('telescope.actions').select_horizontal + require('telescope.actions').center,
+				['<C-v>'] = require('telescope.actions').select_vertical + require('telescope.actions').center,
+				['<C-t>'] = require('telescope.actions').select_tab + require('telescope.actions').center,
 			},
 			n = {
 				['<C-j>'] = 'move_selection_next',
 				['<C-k>'] = 'move_selection_previous',
 				['<C-o>'] = 'select_horizontal',
+				['<CR>'] = require('telescope.actions').select_default + require('telescope.actions').center,
+				['<C-x>'] = require('telescope.actions').select_horizontal + require('telescope.actions').center,
+				['<C-v>'] = require('telescope.actions').select_vertical + require('telescope.actions').center,
+				['<C-t>'] = require('telescope.actions').select_tab + require('telescope.actions').center,
 			}
 		},
 		layout_config = {
