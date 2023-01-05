e0fa9fe42a5705422b79ac79890aa0aefadc6aee
Ofir Gal <ofir@gal.software>
2023-01-05 16:29:33 +0200

    scroll speed

--- awesomewm/autorun.sh
+++ awesomewm/autorun.sh
@@ -3,8 +3,11 @@
 xset r rate 200 35 # Faster repeat rate
 setxkbmap -option caps:escape # Capslock to escape
 
+# Touchpad
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 325 1 # Natural scrolling
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 348 1 # Tapping enabled
+xinput set-prop "DELL0A36:00 0488:101A Touchpad" "libinput Scrolling Pixel Distance" 30
+xinput set-prop "DELL0A36:00 0488:101A Touchpad" "libinput High Resolution Wheel Scroll Enabled" 0
 
 # Mouse speed at home
 xinput --set-prop 'Glorious Model O Wireless' 'libinput Accel Speed' -0.3
