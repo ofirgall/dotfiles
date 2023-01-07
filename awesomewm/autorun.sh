ec40cb99323820b2d404085c84fcf4bd297ac669
Ofir Gal <ofir@gal.software>
2023-01-07 13:01:48 +0200

    scroll speed

--- awesomewm/autorun.sh
+++ awesomewm/autorun.sh
@@ -6,7 +6,7 @@ setxkbmap -option caps:escape # Capslock to escape
 # Touchpad
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 325 1 # Natural scrolling
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" 348 1 # Tapping enabled
-xinput set-prop "DELL0A36:00 0488:101A Touchpad" "libinput Scrolling Pixel Distance" 30
+xinput set-prop "DELL0A36:00 0488:101A Touchpad" "libinput Scrolling Pixel Distance" 40
 xinput set-prop "DELL0A36:00 0488:101A Touchpad" "libinput High Resolution Wheel Scroll Enabled" 0
 
 # Mouse speed at home
