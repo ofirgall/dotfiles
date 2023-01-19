6b8736d24ce14a6640b49e29c7599fc726d3eb8d
Ofir Gal <ofir@gal.software>
2023-01-19 17:53:07 +0200

    works but slow as fuck

--- editors/nvim/lua/plugins/lsp.lua
+++ editors/nvim/lua/plugins/lsp.lua
@@ -166,9 +166,9 @@ lspconfig.sumneko_lua.setup {
 }
 
 -- ofirgall/format-on-leave.nvim
-require('format-on-leave').setup {
-	pattern = { '*.go', '*.rs', '*.lua' }
-}
+-- require('format-on-leave').setup {
+-- 	pattern = { '*.go', '*.rs', '*.lua' }
+-- }
 
 local path = vim.fn.stdpath('config') .. '/spell/en.utf-8.add'
 local words = {}
@@ -279,5 +279,13 @@ require('symbols-outline').setup {
 		Event = { hl = '@type' },
 		Operator = { hl = '@operator' },
 		TypeParameter = { hl = '@parameter' },
+	},
+}
+
+
+require('copilot').setup {
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
 	},
 }
