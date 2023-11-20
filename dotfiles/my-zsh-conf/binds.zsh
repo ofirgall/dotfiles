91f1dcd9f1f71486f71c0aa9556cb09b5556ea08
Ofir Gal <ofir@gal.software>
2023-11-20 15:45:48 +0200

    ctrl+; to complete autosuggestion word

---
+++ dotfiles/my-zsh-conf/binds.zsh
@@ -0,0 +1,1 @@
+bindkey "^[;" forward-word # Ctrl+; to complete go forward a word in order to complete the next word from suggestions
--- dotfiles/my-zsh-conf/plugin_settings.zsh
+++ dotfiles/my-zsh-conf/plugin_settings.zsh
@@ -8,8 +8,3 @@ export CD_TO_GIT_DEFAULT_DIR=~/workspace/work/
 # ---------------------------
 export AUTO_NOTIFY_THRESHOLD=600 # 10 minutes
 export AUTO_NOTIFY_IGNORE=("docker" "man" "sleep" "nvim" "nvlog" "./envctl.py" "pg" "python" "python3" "python2" "viewer" "lsj" "ssh" "rssh" "assh" "gshow" "nv" "v")
-
-# ---------------------------
-#		zsh-autosuggestions
-# ---------------------------
-bindkey "^[;" forward-word # A bind to complete the next word with ctrl+;
