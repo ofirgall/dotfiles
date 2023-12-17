6458ab0029999b9c6c394ee60a911c1bf5d5da59
Ofir Gal <ofir@gal.software>
2023-12-17 20:39:04 +0200

    edit_compile_commands: edit directory

--- scripts/misc/edit_compile_commands.py
+++ scripts/misc/edit_compile_commands.py
@@ -29,5 +29,17 @@ def remove(path: Path, flag: str):
     with open(path, 'w') as file:
         json.dump(commands, file, indent=4)
 
+@app.command()
+def change_dir(path: Path, new_dir: Path):
+    with open(path, 'r') as file:
+        commands = json.load(file)
+
+    for command in commands:
+        command['file'] = command['file'].replace(command['directory'], str(new_dir))
+        command['directory'] = str(new_dir)
+
+    with open(path, 'w') as file:
+        json.dump(commands, file, indent=4)
+
 if __name__ == '__main__':
     app()
