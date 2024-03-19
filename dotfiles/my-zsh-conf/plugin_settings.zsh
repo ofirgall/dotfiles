1fa298f01600e36f0b12bd2525d96e769cce4605
Ofir Gal <ofir@gal.software>
2024-03-19 17:38:19 +0200

    add less to AUTO_NOTIFY_IGNORE

--- dotfiles/my-zsh-conf/plugin_settings.zsh
+++ dotfiles/my-zsh-conf/plugin_settings.zsh
@@ -7,4 +7,4 @@ export CD_TO_GIT_DEFAULT_DIR=~/workspace/work/
 #		zsh-auto-notify
 # ---------------------------
 export AUTO_NOTIFY_THRESHOLD=600 # 10 minutes
-export AUTO_NOTIFY_IGNORE=("docker" "man" "sleep" "nvim" "nvlog" "./envctl.py" "pg" "python" "python3" "python2" "viewer" "lsj" "ssh" "rssh" "assh" "gshow" "nv" "v")
+export AUTO_NOTIFY_IGNORE=("docker" "man" "sleep" "nvim" "nvlog" "./envctl.py" "pg" "python" "python3" "python2" "viewer" "lsj" "ssh" "rssh" "assh" "gshow" "nv" "v" "kv" "less")
