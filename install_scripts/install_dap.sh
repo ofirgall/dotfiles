e9cc8973b0d6c3bda00b726a4ee6c5ad11f90217
Ofir Gal <ofirgal753@gmail.com>
2025-05-01 18:46:27 -0500

    fixed install dap, changed release asset name

--- install_scripts/install_dap.sh
+++ install_scripts/install_dap.sh
@@ -8,9 +8,9 @@ source "$CURRENT_DIR/helpers.sh"
 
 install_codelldb()
 {
-	unzip codelldb-x86_64-linux.vsix
+	unzip codelldb-linux-x64.vsix
 	ln -f -s $HOME/.local/dap/codelldb/extension/adapter/codelldb $HOME/.local/bin/codelldb
 }
 
 # C/C++/Rust debug adapter
-download_latest_release $HOME/.local/dap/codelldb vadimcn/vscode-lldb codelldb-x86_64-linux.vsix install_codelldb
+download_latest_release $HOME/.local/dap/codelldb vadimcn/vscode-lldb codelldb-linux-x64.vsix install_codelldb
