b3817e392a4870590673f6cd97a135a17d242880
Ofir Gal <ofirgal753@gmail.com>
2025-05-01 18:47:22 -0500

    fixed install_spotify

--- install_scripts/install_spotify.sh
+++ install_scripts/install_spotify.sh
@@ -10,24 +10,9 @@ if $IS_REMOTE; then
 fi
 
 
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/spotify.gpg
-echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install -y spotify-client
 
-# Install jpochyla/psst
-rm -rf /tmp/psst
-mkdir /tmp/psst
-cd /tmp/psst
-wget https://nightly.link/jpochyla/psst/workflows/build/master/psst-deb.zip
-
-unzip psst-deb.zip
-
-# libssl1.1
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
-
-sudo dpkg -i *.deb
-
-# Spicetify
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
+# TODO: checkout jpochyla/psst
