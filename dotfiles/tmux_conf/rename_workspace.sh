d26f1674afbdcd68178f4e43732a154e52fe5cba
Ofir Gal <ofirgal753@gmail.com>
2025-05-11 22:39:03 -0500

    hypr: change workspace name by tmux sessions

---
+++ dotfiles/tmux_conf/rename_workspace.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -n "$1" && "$1" == "empty" ]]; then
+    current_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
+    hyprctl dispatch renameworkspace "$current_workspace $current_workspace"
+    exit
+fi
+
+current_session=$(tmux display-message -p '#S')
+echo $current_session
+
+if [[ "$current_session" == *"-viewer" ]]; then
+    exit # Ignore viewer sessions
+fi
+
+current_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
+hyprctl dispatch renameworkspace "$current_workspace $current_workspace T:$current_session"
