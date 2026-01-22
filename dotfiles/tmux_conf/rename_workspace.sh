b61e68b1f37497bc1297e581fe364795b51f3f09
Ofir Gal <ofirgal753@gmail.com>
2026-01-22 16:29:44 +0200

    rename_workspace tmux script adapted to virtualdesk plugin

--- dotfiles/tmux_conf/rename_workspace.sh
+++ dotfiles/tmux_conf/rename_workspace.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+
+CONFIG_LOC="$HOME/.config/hypr/UserConfigs/VirtualDesktopsNames.conf"
+
+function rename_workspace() {
+    local id=$1
+    local name=$2
+
+    local ws_names=$(cat $CONFIG_LOC | sed -n 's/^[[:space:]]*names[[:space:]]*=[[:space:]]*\([^#]*\).*/\1/p')
+
+    ws_names=$(sed -E "s/(^|, )[[:space:]]*$id:[^,]*/\1$id:$name/" <<< "$ws_names")
+
+    cat <<EOF > $CONFIG_LOC
+plugin {
+    virtual-desktops {
+        names = $ws_names
+    }
+}
+EOF
+
+}
+
 if [[ -n "$1" && "$1" == "empty" ]]; then
-    current_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
-    hyprctl dispatch renameworkspace "$current_workspace $current_workspace"
+    current_workspace=$(hyprctl printdesk -j | jq -r '.virtualdesk.id')
+    rename_workspace $current_workspace $current_workspace
     exit
 fi
 
 current_session=$(tmux display-message -p '#S')
-echo $current_session
 
 if [[ "$current_session" == *"-viewer" ]]; then
     exit # Ignore viewer sessions
 fi
 
-current_workspace=$(hyprctl activeworkspace -j | jq -r '.id')
+current_workspace=$(hyprctl printdesk -j | jq -r '.virtualdesk.id')
 icon=""
-hyprctl dispatch renameworkspace "$current_workspace $current_workspace $icon $current_session"
+rename_workspace $current_workspace "$current_workspace $icon $current_session"
