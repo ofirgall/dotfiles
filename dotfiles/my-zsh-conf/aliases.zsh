c3db05914b2dfd175315eaf903612cbb4acea196
Ofir Gal <ofirgal753@gmail.com>
2026-03-08 14:20:35 +0200

    add yazi config

--- dotfiles/my-zsh-conf/aliases.zsh
+++ dotfiles/my-zsh-conf/aliases.zsh
@@ -120,3 +120,13 @@ function notes() {
 	fi
 	cd ~/notes/$session
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
+alias c=y
