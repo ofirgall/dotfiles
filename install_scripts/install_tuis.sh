0435960bddd5122f768f65cc371843ddf5d39e88
Ofir Gal <ofirgal753@gmail.com>
2026-03-28 18:45:28 +0200

    add lumen and hunk

--- install_scripts/install_tuis.sh
+++ install_scripts/install_tuis.sh
@@ -10,3 +10,9 @@ source "$CURRENT_DIR/helpers.sh"
 # https://github.com/tsowell/wiremix
 sudo apt install cargo libpipewire-0.3-dev pkg-config clang
 cargo install wiremix
+
+# https://github.com/modem-dev/hunk
+npm i -g hunkdiff
+
+# https://github.com/jnsahaj/lumen
+cargo install lumen
